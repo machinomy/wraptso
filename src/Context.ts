6ec58e612300dff59e87cc5070758cb3d3b24e24
Sergey Ukustov <sergey.ukustov@machinomy.com>
2019-05-07 22:27:19 +0400

    Initial commit

---
+++ src/Context.ts
@@ -0,0 +1,18 @@
+import { ABIDefinition } from 'web3/eth/abi'
+
+export interface MethodAbi extends ABIDefinition {
+  singleReturnValue: boolean
+}
+
+export interface EventAbi extends ABIDefinition {
+
+}
+
+export default interface Context {
+  artifact: string
+  contractName: string
+  relativeArtifactPath: string
+  getters: Array<MethodAbi>
+  functions: Array<MethodAbi>
+  events: Array<EventAbi>
+}
